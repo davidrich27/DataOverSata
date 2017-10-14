2caaa323e0b27695d87c3d2fa4bb2b5daf908c5b
Esquirethesquire <Derekh@eurekadsl.net>
2017-10-13 19:59:19 -0600

    Made default Frame/Panel View

---
+++ AccountAble/src/DefaultPanel.java
@@ -0,0 +1,64 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import javax.swing.SwingConstants;
+import javax.swing.JToolBar;
+import java.awt.FlowLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.net.URL;
+
+import javax.swing.JPanel;
+
+public class DefaultPanel {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DefaultPanel window = new DefaultPanel();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public DefaultPanel() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame("AccountAble");
+		frame.setIconImage(Toolkit.getDefaultToolkit().getImage("./src/chrome2.png"));
+		frame.setAlwaysOnTop(true);
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.EAST);
+
+		JButton btnLogOff = new JButton("Log Off");
+		panel.add(btnLogOff);
+
+		JLabel lblNewLabel = new JLabel("Developed By: Data Over Sata");
+		frame.getContentPane().add(lblNewLabel, BorderLayout.SOUTH);
+	}
+
+}
