77e76a9385092dd6ddacfd06c09ccbb6b7676b57
Cordell Appel <cordell.appel@umontana.edu>
2017-10-24 10:49:01 -0600

    Create Account
    Header and Footer

--- AccountAble/src/CreateAccount.java
+++ AccountAble/src/CreateAccount.java
@@ -1,20 +1,90 @@
 import java.awt.EventQueue;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.net.URL;
 
-import javax.swing.JPanel;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
-public class CreateAccount {
 
-	private JFrame frame;
+public class CreateAccount extends Application{
+	public static void main(String[] args){
+		launch(args);
+	}
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		//primaryStage.setTitle("Title");
+	//	Button button = new Button();
+	//	button.setText("Click");
+
+	//	BorderPane layout = new BorderPane();
+	//	HBox hbox = addHBox();
+	//	layout.setTop(hbox);
+		//VBox left = new VBox();
+		//layout.setLeft(left);
+		//layout.setLeft(addVBox());
+		//left.getChildren().add(button);
+		//layout.getChildren().add(button);
+		/*addVBox();
+
+		Scene scene = new Scene(layout, 800, 800);
+		primaryStage.setScene(scene);
+		primaryStage.show();*/
+		BorderPane border = new BorderPane();
+
+		HBox header = addHeader();
+		border.setTop(header);
+		HBox footer = addFooter();
+		border.setBottom(footer);
+
+		Scene scene = new Scene(border, 800, 800);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+
+	}
+	public HBox addHeader(){
+		HBox hbox = new HBox();
+		Button b1 = new Button();
+		b1.setText("Logout");
+		b1.setTranslateX(592);
+		Button b2 = new Button();
+		b2.setText("Go Back");
+		b2.setTranslateX(590);
+
+		Text title = new Text("AccountAble");
+		title.setTranslateX(350);
+		hbox.getChildren().addAll(title,b2,b1);
+
+
+		return hbox;
+
+	}
+
+	public HBox addFooter(){
+		HBox hbox = new HBox();
+		Text title = new Text("Developed by Data Over Sata");
+		title.setTranslateX(325);
+		hbox.getChildren().add(title);
+		return hbox;
+	}
+
+
+	/*public VBox addVBox(){
+		VBox vbox = new VBox();
+	}*/
+}
+
+
+
+	/*public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -30,14 +100,14 @@ public class CreateAccount {
 	/**
 	 * Create the application.
 	 */
-	public CreateAccount() {
+	/*public CreateAccount() {
 		initialize();
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
+/*	private void initialize() {
 		frame = new JFrame("AccountAble");
 		frame.setIconImage(Toolkit.getDefaultToolkit().getImage("./src/logo.jpg"));
 		frame.setAlwaysOnTop(true);
@@ -110,4 +180,4 @@ public class CreateAccount {
 
 	}
 
-}
+}*/
