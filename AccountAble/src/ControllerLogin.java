fc77c688a50f1b68b5af023f01adaf25c29cd7ed
ctallwhiteman <ct223134@umconnect.umt.edu>
2017-10-31 23:50:34 -0600

    Update ControllerLogin.java
    Striped code out.

--- AccountAble/src/ControllerLogin.java
+++ AccountAble/src/ControllerLogin.java
@@ -1,22 +1,4 @@
-import java.awt.Label;
-import java.awt.TextField;
-import java.awt.event.ActionEvent;
-
-import javafx.fxml.FXML;
 
 public class ControllerLogin {
-	@FXML
-	private Label lblStatus;
-	@FXML
-	private TextField txtUsername;
-	@FXML
-	private TextField txtPassword;
 
-	public void Login(ActionEvent event) {
-		if (txtUsername.getText().equals("user")&& txtPassword.getText().equals("pass")) {
-			lblStatus.setText("Login Success");
-		}else {
-			lblStatus.setText("Login Failed");
-		}
-	}
 }
